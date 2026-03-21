babdf039087b68e9986e69ddab5f6d7e74882254
24A31A42A4 <rohitvijayadapa20062gmail.com>
2026-03-21 11:05:07 +0530

    fix vite host issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   envDir: '../',
@@ -12,8 +11,6 @@ export default defineConfig({
 
   preview: {
     host: true,
-    allowedHosts: [
-      'autobot-form-filler-vijay.up.railway.app'
-    ]
+    allowedHosts: 'all'
   }
 })
